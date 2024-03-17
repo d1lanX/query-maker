7fea2554f0b96cfb4cd47b88231b248fb8166ad4
d1lanX <rodriguezdylan58@gmail.com>
2024-03-16 21:44:27 -0500

    sql keywords added

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,7 @@
-// place files you want to import through the `$lib` alias in this folder.
+export const keywords: string[] = [
+  'select','insert','update','delete','create','drop', 'and',
+  'is', 'set','alter','join','where','group by','order by','having',
+  'distinct','limit','offset','union','in','exists',
+  'not','between','like','ilike','as','cascade',
+  'primary key','foreign key','unique','default','serial','from'
+];
