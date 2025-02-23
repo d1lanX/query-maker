d2b8d05b98768e682fc9ea53bced1dec71e9469d
d1lanX <rodriguezdylan58@gmail.com>
2025-02-22 22:46:48 -0500

    nueva keyword

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,7 @@
 export const keywords: string[] = [
-  'select','insert','update','delete','create','drop', 'and',
-  'is', 'set','alter','join','where','group by','order by','having',
-  'distinct','limit','offset','union','in','exists',
-  'not','between','like','ilike','as','cascade',
-  'primary key','foreign key','unique','default','serial','from'
+  'select', 'insert', 'update', 'delete', 'create', 'drop', 'and',
+  'is', 'set', 'alter', 'join', 'where', 'group by', 'order by', 'having',
+  'distinct', 'limit', 'offset', 'union', 'in', 'exists',
+  'not', 'between', 'like', 'ilike', 'as', 'cascade',
+  'primary key', 'foreign key', 'unique', 'default', 'serial', 'from', 'into'
 ];
