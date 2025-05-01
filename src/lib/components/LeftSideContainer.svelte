790684b341919bbf778d7680b2fe1f13421c0fde
d1lanX <rodriguezdylan58@gmail.com>
2025-05-01 13:00:41 -0500

    feat: ux changes

--- src/lib/components/LeftSideContainer.svelte
+++ src/lib/components/LeftSideContainer.svelte
@@ -7,16 +7,10 @@
   <h1 class="text-gray-600">genera una nueva query a partir de un archivo</h1>
   {@render fileReader()}
   <section class="mt-5">
-    <h1 class="text-xl text-gray-700 font-bold">columnas</h1>
-    <small class="mt-1 mb-5 text-gray-700">
-      reemplaza las columnas que deseas por los campos con signo $ (si quieres que salgan entre
-      comillas como string) o # (si quieres que salgan como dato numerico)
-      <br />
-    </small>
-    <code class="text-gray-500 text-xs"
-      >Ejemplo: UPDATE users SET user_name = $1, user_address = $2, age = #3 WHERE user_id = $4</code
-    >
-    <hr class="my-6" />
+    <h1 class="text-xl text-gray-700 font-bold bg-gray-100 p-2 flex items-center gap-2">columnas</h1>
+    <hr>
     {@render htmlColumns()}
   </section>
 </section>
+
+
--- src/lib/components/QueryWriter.svelte
+++ src/lib/components/QueryWriter.svelte
@@ -28,8 +28,18 @@
 </script>
 
 <section class="mb-12">
-  <header class="text-xl bg-gray-200 rounded p-2 font-bold text-gray-700">nueva query</header>
-  <footer class="flex mt-5">
+  <header class="text-xl bg-gray-100 rounded p-2 font-bold text-gray-700">nueva query</header>
+  <details class="p-2 border rounded cursor-pointer my-2">
+    <summary class="text-xs text-gray-500">cómo hago mi query?</summary>
+    <p class="text-gray-500 text-xs mt-4">
+      UPDATE users SET user_name = $1, user_address = $2, age = #3 WHERE user_id = $4
+    </p>
+    <small class="text-gray-500 text-xs italic"
+      >utiliza los números de columnas en la izquierda para indicar qué datos usar, <mark>$</mark>
+      para texto y <mark>#</mark> para números</small
+    >
+  </details>
+  <footer class="flex mt-2">
     <label class="flex-grow">
       <input
         oninput={uppercaseKeywords}
